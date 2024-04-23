8258b4bb0d172d9187a1f3776440b17e4eafcd5b
Tetiana Parshakova <tetianap@stanford.edu>
2024-04-23 15:51:11 -0700

    jaewook's code for prox terms closed form

--- convergence/decentr_geom_median.py
+++ convergence/decentr_geom_median.py
@@ -10,21 +10,31 @@ import cvxpy as cp
 
 
 
-def prox_fj_geom_mean(z, alpha, b):
+def prox_fj_geom_mean(z, rho, b):
     """
-    return minimizer to \|x - b\|_2 + (1/(2 * alpha)) * \|x - z\|_2^2
+    return minimizer to \|x - b\|_2 + (1/(2 * rho)) * \|x - z\|_2^2
     """
     e = b - z
-    x = b - (e / np.linalg.norm(e)) * np.maximum(np.linalg.norm(e) - alpha, 0.)
+    x = b - (e / np.linalg.norm(e)) * np.maximum(np.linalg.norm(e) - rho, 0.)
     return x
 
-
-def cvx_prox_fj_geom_mean(z, alpha, b, vector_size):
+def prox_fj_geom_mean_sc(z, rho, b, c, theta=1e-4):
     """
-    return minimizer to \|x - b\|_2 + (1/(2 * alpha)) * \|x - z\|_2^2
+    return minimizer to \|x - b\|_2 + theta * \|x - c\|_2^2 + (1/(2 * rho)) * \|x - z\|_2^2
+    """
+    new_z = 1/(1 + 2 * rho * theta) * ( z + 2 * rho * theta * c)
+    new_rho = rho/(1 + 2 * rho * theta)
+    # e = b - new_z
+    # x = b - (e / np.linalg.norm(e)) * np.maximum(np.linalg.norm(e) - new_rho, 0.)
+    x = prox_fj_geom_mean(new_z, new_rho, b)
+    return x
+
+def cvx_prox_fj_geom_mean(z, rho, b, vector_size):
+    """
+    return minimizer to \|x - b\|_2 + (1/(2 * rho)) * \|x - z\|_2^2
     """
     x = cp.Variable(vector_size)
-    f = cp.norm(x - b, 2) + (1/(2*alpha)) * cp.sum_squares(x - z)
+    f = cp.norm(x - b, 2) + (1/(2*rho)) * cp.sum_squares(x - z)
 
     prob = cp.Problem(cp.Minimize(f), [])
     prob.solve()
@@ -32,12 +42,12 @@ def cvx_prox_fj_geom_mean(z, alpha, b, vector_size):
     return x.value
 
 
-def cvx_prox_fj_geom_mean_sc(z, alpha, b, vector_size, eps=1e-4):
+def cvx_prox_fj_geom_mean_sc(z, rho, b, c, vector_size, theta=1e-4):
     """
-    return minimizer to \|x - b\|_2 + eps * \|x\|_2^2 + (1/(2 * alpha)) * \|x - z\|_2^2
+    return minimizer to \|x - b\|_2 + theta * \|x - c\|_2^2 + (1/(2 * rho)) * \|x - z\|_2^2
     """
     x = cp.Variable(vector_size)
-    f = cp.norm(x - b, 2)  + eps * cp.sum_squares(x) + (1/(2*alpha)) * cp.sum_squares(x - z)
+    f = cp.norm(x - b, 2)  + theta * cp.sum_squares(x-c) + (1/(2*rho)) * cp.sum_squares(x - z)
 
     prob = cp.Problem(cp.Minimize(f), [])
     prob.solve()
@@ -48,17 +58,19 @@ def cvx_prox_fj_geom_mean_sc(z, alpha, b, vector_size, eps=1e-4):
 def cvx_geom_median(problem_spec, problem_data):
     n_node = problem_spec['n_node']
     vector_size = problem_spec['vector_size']
-    eps = problem_spec['sc_eps']
+    theta = problem_spec['sc_theta']
     b = problem_data['b']
+    c = problem_data['c']
 
     x = cp.Variable(vector_size)
     f = 0
 
     for jj in range(n_node) :
         b_temp = b[jj*vector_size : (jj+1)*vector_size]
+        c_temp = c[jj*vector_size : (jj+1)*vector_size]
         f += cp.norm(x - b_temp, 2)  
         if jj in [3, 4]:
-            f += eps * cp.sum_squares(x) 
+            f += theta * cp.sum_squares(x-c_temp) 
 
     prob = cp.Problem(cp.Minimize(f), [])
     prob.solve()
@@ -70,12 +82,20 @@ def cvx_geom_median(problem_spec, problem_data):
 def data_generation(problem_spec) :
     n_node = problem_spec['n_node']
     vector_size = problem_spec['vector_size']
+    translation = problem_spec['translation']
 
     # vectors which geom. median we want to find
     # b = np.random.randn(n_node * vector_size)
     b = np.random.rand(n_node * vector_size) * 200 - 100
+    
+    if translation=="b":
+        c = b
+    elif translation=="rand":
+        c = np.random.rand(n_node * vector_size) * 200 - 100
+    else:
+        c = np.zeros(n_node * vector_size)
 
-    problem_data = {'b' : b}
+    problem_data = {'b' : b, 'c' : c}
     return problem_data
 
 
@@ -110,20 +130,20 @@ def graph_generation_nodes6():
 def p_extra_dgeom_median(method_ver, problem_spec, problem_data, network_data, x_opt_star, f_star, printing=False, freq=200, params=None) :
     n_node = problem_spec['n_node']
     vector_size = problem_spec['vector_size']
-    alpha = problem_data['alpha']
-    eps = problem_spec['sc_eps']
+    rho = problem_data['rho']
+    theta = problem_spec['sc_theta']
 
     b = problem_data['b']
+    c = problem_data['c']
     itr_num = problem_data['itr_num']
     W = network_data['W']
     
     err_opt_star, err_opt_reldiff, op_norm, const_vio, f_reldiff = [], [], [], [], []
 
-    x_0 = np.zeros((n_node,vector_size))
+    x_0 = np.reshape(b, (n_node, vector_size))
     x_k = np.array(x_0)
     w_0 = np.zeros((n_node,vector_size))
     w_k = np.array(w_0)
-    # b_stack = np.reshape(np.repeat(b, n_node), (n_sensor, n_node))
 
     e_k = np.array(x_0)
 
@@ -133,18 +153,20 @@ def p_extra_dgeom_median(method_ver, problem_spec, problem_data, network_data, x
         f_val = 0
         for jj in range(n_node) :
             b_temp = b[jj*vector_size : (jj+1)*vector_size]
+            c_temp = c[jj*vector_size : (jj+1)*vector_size]
             z_temp = (W[jj]@x_k_prev - w_k_prev[jj])
             if jj in [3, 4]:
-                x_k[jj] = cvx_prox_fj_geom_mean_sc(z_temp, alpha, b_temp, vector_size, eps=eps)
-                f_val += np.linalg.norm((x_k[jj] - b_temp), ord=2) + eps * np.linalg.norm(x_k[jj], ord=2)**2
+                x_k[jj] = prox_fj_geom_mean_sc(z_temp, rho, b_temp, c_temp, theta=theta)
+                # x_k[jj] = cvx_prox_fj_geom_mean_sc(z_temp, rho, b_temp, c_temp, vector_size, theta=theta)
+                f_val += np.linalg.norm((x_k[jj] - b_temp), ord=2) + theta * np.linalg.norm(x_k[jj] - c_temp, ord=2)**2
             else:
                 e_k[jj] = b_temp - z_temp
-                x_k[jj] = b_temp - (e_k[jj] / np.linalg.norm(e_k[jj])) * np.maximum(np.linalg.norm(e_k[jj])-alpha, 0.)
+                x_k[jj] = prox_fj_geom_mean(z_temp, rho, b_temp)
                 f_val += np.linalg.norm((x_k[jj] - b_temp), ord=2)
 
         w_k = w_k_prev + 1/2*(np.eye(n_node) - W) @ x_k_prev
         
-        op_norm.append(Mnormsq(x_k-x_k_prev, w_k-w_k_prev, alpha, network_data, n_node))        
+        op_norm.append(Mnormsq(x_k-x_k_prev, w_k-w_k_prev, rho, network_data, n_node))        
 
         err_opt_star.append(np.sqrt(np.sum((x_k - x_opt_star)**2)))
         err_opt_reldiff.append(np.sqrt(np.sum((x_k - x_opt_star)**2)) / np.sqrt(np.sum((x_0 - x_opt_star)**2)))
@@ -156,26 +178,26 @@ def p_extra_dgeom_median(method_ver, problem_spec, problem_data, network_data, x
     return op_norm, err_opt_star, err_opt_reldiff, const_vio, f_reldiff
 
 """
-    Helper functions: calculates inner product and norm induced by metric matrix M (which is dependent on alpha).
+    Helper functions: calculates inner product and norm induced by metric matrix M (which is dependent on rho).
 
     (1) Minner  : calculates M-inner product between (x_1, w_1) and (x_2, w_2)
     (2) Mnormsq : calculates squared M-norm of (x, w)
     (3) Mnorm   : calculates M-norm of (x, w)
 """
 
-def Minner(x1, w1, x2, w2, alpha, network_data, n_node) :
+def Minner(x1, w1, x2, w2, rho, network_data, n_node) :
     Vred = network_data['Vred']
     Sred = network_data['Sred']
 
-    norm_squared = (1/alpha)*np.sum(x1*x2) + (1/alpha)*np.sum(x1*w2) + (1/alpha)*np.sum(w1*x2)
-    u1 = (1/alpha)*Vred.T@((Vred@w1)*np.reshape(1/np.sqrt(Sred), (n_node-1,1)))
-    u2 = (1/alpha)*Vred.T@((Vred@w2)*np.reshape(1/np.sqrt(Sred), (n_node-1,1)))
-    norm_squared = norm_squared + alpha*np.sum(u1*u2)
+    norm_squared = (1/rho)*np.sum(x1*x2) + (1/rho)*np.sum(x1*w2) + (1/rho)*np.sum(w1*x2)
+    u1 = (1/rho)*Vred.T@((Vred@w1)*np.reshape(1/np.sqrt(Sred), (n_node-1,1)))
+    u2 = (1/rho)*Vred.T@((Vred@w2)*np.reshape(1/np.sqrt(Sred), (n_node-1,1)))
+    norm_squared = norm_squared + rho*np.sum(u1*u2)
 
     return norm_squared
 
-def Mnormsq(x, w, alpha, network_data, n_node) :
-    return 1/4*Minner(x, w, x, w, alpha, network_data, n_node)
+def Mnormsq(x, w, rho, network_data, n_node) :
+    return 1/4*Minner(x, w, x, w, rho, network_data, n_node)
 
-def Mnorm(x, w, alpha, network_data, n_node) :
-    return 1/2*np.sqrt(Minner(x, w, x, w, alpha, network_data, n_node))
+def Mnorm(x, w, rho, network_data, n_node) :
+    return 1/2*np.sqrt(Minner(x, w, x, w, rho, network_data, n_node))
