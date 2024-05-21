08e6b1745e7db499ea3f8fbbae266f7ba1ba1ae9
Tetiana Parshakova <tetianap@stanford.edu>
2024-05-21 15:12:56 -0700

    started new experiment

--- ciropt/discretized_methods.py
+++ ciropt/discretized_methods.py
@@ -9,7 +9,7 @@ import cvxpy as cp
 
 def dadmm(alg_type, problem_spec, problem_data, network_data, x_opt_star, f_star, 
           prox_operators, fi_operators, params=None, printing=False, freq=50):
-    n_node = problem_spec['n_node']
+    n_nodes = problem_spec['n_nodes']
     vector_size = problem_spec['vector_size']
     
     # x_0_data = problem_data['x_0_data']
@@ -26,53 +26,37 @@ def dadmm(alg_type, problem_spec, problem_data, network_data, x_opt_star, f_star
     elif alg_type == "cir_dadmm":
         R, h, Inductance = params["R"], params["h"], params["Inductance"]
         step_size = h / Inductance
-    elif alg_type == "cir_dadmm_l":
-        R, h, Inductance, L_extra = params["R"], params["h"], params["Inductance"], params["L_extra"]
-        step_size = h / Inductance
     elif alg_type == "cir_dadmm_c":
         R, h, Inductance, Capacitance = params["R"], params["h"], params["Inductance"], params["Capacitance"]
         step_size = h / Inductance
 
-    err_opt_star, err_opt_reldiff, op_norm, const_vio, f_reldiff = [], [], [], [], []
+    err_opt_star, err_opt_reldiff, const_vio, f_reldiff = [], [], [], []
 
-    x_0 = np.zeros((n_node, vector_size))
+    x_0 = np.zeros((n_nodes, vector_size))
     x_k = np.array(x_0)
     i_L_k = [np.zeros((deg, vector_size)) for deg in node_degrees]
     e_k = [np.zeros((deg, vector_size)) for deg in node_degrees]
-    
-    if alg_type == "cir_dadmm_l":
-        i_Lextra_k = np.zeros((1, vector_size))
 
     for ii in range(itr_num):
         if ii >= 1: x_k_prev = np.array(x_k)
         i_L_k_prev = [np.array(el) for el in i_L_k]
         e_k_prev = [np.array(el) for el in e_k]
-        if alg_type == "cir_dadmm_l":
-            i_Lextra_k_prev = np.array(i_Lextra_k)
         f_val = 0
 
-        for jj in range(n_node):
-            if alg_type == "cir_dadmm_l" and jj == 3:
-                zj = (1./node_degrees[jj]) * (R * i_L_k_prev[jj] + e_k_prev[jj] - R * i_Lextra_k_prev).sum(axis=0)
-            elif alg_type == "cir_dadmm_l" and jj == 4:
-                zj = (1./node_degrees[jj]) * (R * i_L_k_prev[jj] + e_k_prev[jj] + R * i_Lextra_k_prev).sum(axis=0)
-            else:
-                zj = (1./node_degrees[jj]) * (R * i_L_k_prev[jj] + e_k_prev[jj]).sum(axis=0)
+        for jj in range(n_nodes):
+            zj = (1./node_degrees[jj]) * (R * i_L_k_prev[jj] + e_k_prev[jj]).sum(axis=0)
             x_k[jj] = prox_operators[jj](zj, R/node_degrees[jj])
             f_val += fi_operators[jj](x_k[jj])
 
-        for j in range(n_node):
+        for j in range(n_nodes):
             for l_idx, l in enumerate(adjacency[j]):
                 if alg_type == "cir_dadmm_c" and ((j == 3 and l==4) or (j==4 and l==3)):
                     j_idx = adjacency[l].index(j)
                     e_k[j][l_idx] = e_k_prev[j][l_idx] - (h / Capacitance) * (i_L_k_prev[j][l_idx] + i_L_k_prev[l][j_idx] + (2 * e_k_prev[j][l_idx] - x_k[j] - x_k[l])/R)
                 else:
                     e_k[j][l_idx] = (1/2) * (x_k[j] + x_k[l])
-        for j in range(n_node):
+        for j in range(n_nodes):
             i_L_k[j] = i_L_k_prev[j] + step_size * ( e_k[j] - x_k[j])
-            
-        if alg_type == "cir_dadmm_l":
-            i_Lextra_k = i_Lextra_k_prev + (h / L_extra) * (x_k[3] - x_k[4])
         
         err_opt_star.append(np.sqrt(np.sum((x_k - x_opt_star)**2)))
         err_opt_reldiff.append(np.sqrt(np.sum((x_k - x_opt_star)**2)) / np.sqrt(np.sum((x_0 - x_opt_star)**2)))
---
+++ opt_problems/dadmm_experiment.py
@@ -0,0 +1,171 @@
+"""
+    Some functions are from  https://github.com/jaewookjsuh/continuous_anchor_acceleration_experiment/tree/main 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import cvxpy as cp
+
+
+
+
+def prox_fj_geom_mean(z, rho, b):
+    """
+    return minimizer to \|x - b\|_2 + (1/(2 * rho)) * \|x - z\|_2^2
+    """
+    e = b - z
+    x = b - (e / np.linalg.norm(e)) * np.maximum(np.linalg.norm(e) - rho, 0.)
+    return x
+
+def cvx_prox_fj_geom_mean(z, rho, b, vector_size):
+    """
+    return minimizer to \|x - b\|_2 + (1/(2 * rho)) * \|x - z\|_2^2
+    """
+    x = cp.Variable(vector_size)
+    f = cp.norm(x - b, 2) + (1/(2*rho)) * cp.sum_squares(x - z)
+
+    prob = cp.Problem(cp.Minimize(f), [])
+    prob.solve()
+    assert prob.status=="optimal", print(prob.status)
+    return x.value
+
+
+def prox_fj_geom_mean_sc(z, rho, b, P):
+    """
+    return minimizer to  (1/2)\|x - b\|_P^2 + (1/(2 * rho)) * \|x - z\|_2^2
+    """
+    tilde_P = P + np.eye(P.shape[0]) / rho
+    new_z = b + np.linalg.solve(tilde_P, (z - b) / rho )
+    return new_z
+
+def cvx_prox_fj_geom_mean_sc(z, rho, b, P, vector_size):
+    """
+    return minimizer to  (1/2)\|x - b\|_P^2 + (1/(2 * rho)) * \|x - z\|_2^2
+    """
+    x = cp.Variable(vector_size)
+    f = 0.5 * cp.quad_form(x - b, P) + (1/(2*rho)) * cp.sum_squares(x - z)
+
+    prob = cp.Problem(cp.Minimize(f), [])
+    prob.solve()
+    assert prob.status=="optimal", print(prob.status)
+    return x.value
+
+
+def cvx_geom_median(problem_spec, problem_data):
+    n_nodes = problem_spec['n_nodes']
+    vector_size = problem_spec['vector_size']
+    b = problem_data['b']
+    P = problem_data['P']
+
+    x = cp.Variable(vector_size)
+    f = 0
+
+    for jj in range(n_nodes) :
+        bj = b[jj*vector_size : (jj+1)*vector_size]
+        Pj = P[jj]  
+        if jj in [3, 4]:
+            f += 0.5 * cp.quad_form(x - bj, Pj) 
+        else:
+            f += cp.norm(x - bj, 2)
+
+    prob = cp.Problem(cp.Minimize(f), [])
+    prob.solve()
+    assert prob.status=="optimal"
+    x_cvx = np.repeat(x.value, n_nodes).reshape(vector_size, n_nodes).T
+    return f.value, x_cvx
+
+
+def data_generation(problem_spec) :
+    n_nodes = problem_spec['n_nodes']
+    vector_size = problem_spec['vector_size']
+
+    # vectors which geom. median we want to find
+    # b = np.random.randn(n_nodes * vector_size)
+    b = np.random.rand(n_nodes * vector_size) * 200 - 100
+    
+    eigs = 1 + 10**np.linspace(0, 3, vector_size, endpoint=True)
+    Ps = []
+    for _ in range(n_nodes):
+        Q = np.linalg.qr(np.random.randn(vector_size, vector_size))[0]
+        P = (Q * eigs) @ Q.T
+        assert np.allclose(eigs, np.sort(np.linalg.eigvalsh(P)))
+        Ps += [P]
+
+    problem_data = {'b' : b, 'P' : Ps}
+    return problem_data
+
+
+def graph_generation_nodes6():
+    # graph with 6 nodes and 7 edges (1,2), (1,3), (2,3), (2,4), (3,4), (4,5), (4,6)
+    # 1 -- 2 -- 4 -- 5
+    #   \  |  /  \
+    #      3       6 
+    n_nodes = 6
+
+    # (1) network
+    G = nx.Graph()
+    G.add_nodes_from([1, n_nodes])
+    G.add_edges_from([(1,2), (1,3), (2,3), (2,4), (3,4), (4,5), (4,6)])
+
+    # (2) mixing matrices : metropolis-hastings weights
+    W = np.zeros((n_nodes, n_nodes))
+    for edge in list(G.edges) :
+        (i,j) = edge
+        W[i-1,j-1] = 1/(np.maximum(G.degree(i), G.degree(j))+1)
+        W[j-1,i-1] = 1/(np.maximum(G.degree(j), G.degree(i))+1)
+    for i in range(n_nodes) :
+        W[i,i] = 1 - np.sum(W[i])
+    [_, S, V] = np.linalg.svd((1/2)*(np.eye(W.shape[0])-W))
+    Vred = V[0:n_nodes-1]
+    Sred = S[0:n_nodes-1]
+
+    network_data = {'G' : G, 'W' : W, 'Vred' : Vred, 'Sred' : Sred}
+    return network_data
+
+
+def p_extra_dgeom_median(method_ver, problem_spec, problem_data, network_data, x_opt_star, f_star, printing=False, freq=200) :
+    n_nodes = problem_spec['n_nodes']
+    vector_size = problem_spec['vector_size']
+    rho = problem_data['rho']
+
+    b = problem_data['b']
+    P = problem_data['P']
+    itr_num = problem_data['itr_num']
+    W = network_data['W']
+    
+    err_opt_star, err_opt_reldiff, const_vio, f_reldiff = [], [], [], []
+
+    x_0 = np.reshape(b, (n_nodes, vector_size))
+    x_k = np.array(x_0)
+    w_0 = np.zeros((n_nodes,vector_size))
+    w_k = np.array(w_0)
+
+    e_k = np.array(x_0)
+
+    for ii in range(itr_num) :
+        x_k_prev = np.array(x_k)
+        w_k_prev = np.array(w_k)
+        f_val = 0
+        for jj in range(n_nodes) :
+            bj = b[jj*vector_size : (jj+1)*vector_size]
+            Pj = P[jj]
+            z_temp = (W[jj]@x_k_prev - w_k_prev[jj])
+            if jj in [3, 4]:
+                x_k[jj] = prox_fj_geom_mean_sc(z_temp, rho, bj, Pj) 
+                f_val += 0.5 * (x_k[jj] - bj).T @ Pj @ (x_k[jj] - bj)
+            else:
+                e_k[jj] = bj - z_temp
+                x_k[jj] = prox_fj_geom_mean(z_temp, rho, bj)
+                f_val += np.linalg.norm((x_k[jj] - bj), ord=2)
+
+        w_k = w_k_prev + 1/2*(np.eye(n_nodes) - W) @ x_k_prev
+              
+        err_opt_star.append(np.sqrt(np.sum((x_k - x_opt_star)**2)))
+        err_opt_reldiff.append(np.sqrt(np.sum((x_k - x_opt_star)**2)) / np.sqrt(np.sum((x_0 - x_opt_star)**2)))
+        f_reldiff.append(np.abs(f_star - f_val)/f_star)
+        if printing and (ii % freq == 0 or ii == itr_num-1):
+            print(f"{ii=}, {f_reldiff[-1]=}, {err_opt_reldiff[-1]=}")
+
+    return err_opt_star, err_opt_reldiff, const_vio, f_reldiff
+
